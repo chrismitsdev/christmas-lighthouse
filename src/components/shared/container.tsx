3bbbb995a7903017867d85008f12856b70443cca
chrismitsdev <chrismits88@gmail.com>
2024-11-08 13:00:30 +0200

    ref as prop in container

--- src/components/shared/container.tsx
+++ src/components/shared/container.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import {Slot} from '@radix-ui/react-slot'
 import {cn} from '@/src/lib/utils'
 
-type ContainerProps = React.ComponentPropsWithoutRef<'div'> & {
-  ref: 
-}
-
-function Container2({ref, ...props}: React.ComponentPropsWithoutRef<'div'>) {}
-
-const Container = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & AsChild
->(({asChild = false, className, ...props}, ref) => {
+function Container({
+  className,
+  asChild = false,
+  ref,
+  ...props
+}: React.ComponentPropsWithRef<'div'> & AsChild) {
   const Comp = asChild ? Slot : 'div'
 
   return (
@@ -21,7 +17,22 @@ const Container = React.forwardRef<
       {...props}
     />
   )
-})
+}
+
+// const Container = React.forwardRef<
+//   HTMLDivElement,
+//   React.HTMLAttributes<HTMLDivElement> & AsChild
+// >(({asChild = false, className, ...props}, ref) => {
+//   const Comp = asChild ? Slot : 'div'
+
+//   return (
+//     <Comp
+//       className={cn('container max-w-7xl', className)}
+//       ref={ref}
+//       {...props}
+//     />
+//   )
+// })
 
 Container.displayName = 'Container'
 
