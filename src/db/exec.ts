8a6388ac92d7af4e5ef7fc9fdf16bfc11f17f97f
chrismitsdev <chrismits88@gmail.com>
2024-11-23 12:34:04 +0200

    fixed type error in exec.ts

--- src/db/exec.ts
+++ src/db/exec.ts
@@ -8,6 +8,12 @@ async function main() {
   const testEmail = 'chrismits88@gmail.com'
 
   const user = await getUserFromEmail(testEmail)
+
+  if (user === null) {
+    console.log('A user with this email was not found')
+    return
+  }
+
   const passwordHash = await getUserPasswordHash(user.id)
   const validPassword = await verifyPasswordHash(passwordHash, '2551022619aA1')
 
