1e4f17af5d7796e298290f097e2f38acfd5ca817
chrismitsdev <chrismits88@gmail.com>
2025-12-05 22:26:27 +0200

    chore: seo optimizations, video change

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/'
     },
-    sitemap: `https://thechristmaslighthouse.gr/sitemap.xml`
+    sitemap: 'https://thechristmaslighthouse.gr/sitemap.xml'
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,43 +1,19 @@
 import type {MetadataRoute} from 'next'
-import type {Locale} from 'next-intl'
 import {getPathname} from '@/src/i18n/navigation'
-import {routing} from '@/src/i18n/routing'
 
-type Href = Parameters<typeof getPathname>[0]['href']
-
-const slugs = [
-  'coffee',
-  'beverage',
-  'refreshment',
-  'energy',
-  'spirit',
-  'cocktail',
-  'beer',
-  'food',
-  'pizza',
-  'burger',
-  'salad',
-  'snack'
-]
 const host = 'https://thechristmaslighthouse.gr'
 
-function getEntry(href: Href): MetadataRoute.Sitemap[0] {
-  return {
-    url: getUrl(href, routing.defaultLocale),
-    lastModified: new Date(),
-    alternates: {
-      languages: Object.fromEntries(
-        routing.locales.map((locale) => [locale, getUrl(href, locale)])
-      )
-    }
-  }
-}
-
-function getUrl(href: Href, locale: Locale) {
-  const pathname = getPathname({locale, href})
-  return host + pathname
-}
-
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [getEntry('/'), ...slugs.map((s) => getEntry(`/${s}`))]
+  return [
+    {
+      url: host,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          el: host + getPathname({locale: 'el', href: '/'}),
+          en: host + getPathname({locale: 'en', href: '/'})
+        }
+      }
+    }
+  ]
 }
