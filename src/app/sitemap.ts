ec76ba4ca1fe34b60fa2d94c495c22cc955af9fa
chrismitsdev <chrismits88@gmail.com>
2025-12-06 19:41:01 +0200

    chore: add menu to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,12 @@
 import type {MetadataRoute} from 'next'
+import {getCategories} from '@/src/db/menu'
 import {getPathname} from '@/src/i18n/navigation'
 
 const host = 'https://thechristmaslighthouse.gr'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const categories = await getCategories()
+
   return [
     {
       url: host,
@@ -14,6 +17,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
           en: host + getPathname({locale: 'en', href: '/'})
         }
       }
-    }
+    },
+    {
+      url: `${host}/menu`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          el: `${host}${getPathname({locale: 'el', href: '/'})}/menu`,
+          en: `${host}${getPathname({locale: 'en', href: '/'})}/menu`
+        }
+      }
+    },
+    ...categories.map(({id}) => ({
+      url: `${host}/menu/${id}`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          el: `${host}${getPathname({locale: 'el', href: '/'})}/menu/${id}`,
+          en: `${host}${getPathname({locale: 'en', href: '/'})}/menu/${id}`
+        }
+      }
+    }))
   ]
 }
