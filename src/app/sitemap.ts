d9f2b59b12024a1808953f4fab01805119578dab
chrismitsdev <chrismits88@gmail.com>
2025-12-11 02:29:24 +0200

    chore: minor fix in sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,16 @@
 import type {MetadataRoute} from 'next'
-import {getCategories} from '@/src/db/menu'
 import {getPathname} from '@/src/i18n/navigation'
 
 const host = 'https://thechristmaslighthouse.gr'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const categories = await getCategories()
-
   return [
     {
       url: host,
       lastModified: new Date(),
       alternates: {
         languages: {
+          'x-default': host,
           el: host + getPathname({locale: 'el', href: '/'}),
           en: host + getPathname({locale: 'en', href: '/'})
         }
@@ -26,26 +24,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
           content_loc: `${host}/sections/parade/video/parade-video.mp4`
         }
       ]
-    },
-    {
-      url: `${host}/menu`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          el: `${host}${getPathname({locale: 'el', href: '/'})}/menu`,
-          en: `${host}${getPathname({locale: 'en', href: '/'})}/menu`
-        }
-      }
-    },
-    ...categories.map(({id}) => ({
-      url: `${host}/menu/${id}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          el: `${host}${getPathname({locale: 'el', href: '/'})}/menu/${id}`,
-          en: `${host}${getPathname({locale: 'en', href: '/'})}/menu/${id}`
-        }
-      }
-    }))
+    }
   ]
 }
