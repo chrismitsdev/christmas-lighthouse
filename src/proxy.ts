23fd00ce69fd69b7e18011bc6f626abeb07b1464
chrismitsdev <chrismits88@gmail.com>
2025-12-19 21:28:29 +0200

    chore: minor fix in proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -14,7 +14,8 @@ export default async function middleware(
   const response = nextIntlMiddleware(request)
 
   // Only extend the cookie expiration on GET requests.
-  if (request.method === 'GET') {
+  // if (request.method === 'GET') {
+  if (request.method === 'GET' || request.method === 'HEAD') {
     const token = request.cookies.get('session')?.value ?? null
 
     if (token !== null) {
